91e5ad611903abec7df89a6ab6d00f1972c4caed
MarcoAntonioMaschio <marco.antonio.maschio@gmail.com>
2021-10-18 23:02:51 -0300

    Primeira Aula

---
+++ js/main.js
@@ -0,0 +1,78 @@
+var d = new Date();
+
+alert(d.getMinutes());
+alert(d.getDay());
+alert(d.getMonth());
+alert(d.getDate());
+
+
+
+/*
+var count;
+
+for (count = 3; count <=5; count++){
+  alert(count);
+}
+
+
+
+var count = 0;
+while (count <= 5) {
+  console.log(count);
+  alert(count);
+  count++;
+}
+
+var idade = prompt("Qual a sua Idade?")
+//var idade = 18;
+
+if (idade >= 18){
+  alert("Maior de Idade");
+}
+else {
+  alert("Menor de Idade");
+}
+
+
+
+var nome = "Marco Antonio Maschio";
+var idade = 29;
+var idade2 = 10;
+var n1 = 5;
+var n2 = 10;
+var lista = ["maça", "pera", "laranja"]
+var fruta = {nome: "maça", cor: "vermelha"}
+var frase = "Japão é o melhor time do mundo!";
+//alert(nome + " tem " + idade + " anos.");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+console.log(n1 * n2);
+console.log(lista);
+console.log(lista[1]);
+//alert(lista[2]);
+lista.push("uva");
+console.log(lista);
+lista.pop();
+console.log(lista);
+console.log(lista.length);
+lista.push("uva");
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista[0]);
+console.log(lista.toString()[0]);
+console.log(lista.join(" | "));
+console.log(fruta.nome);
+console.log(fruta.cor);
+
+
+var fruta = [{nome: "maça", cor: "vermelha"},{nome: "uva", cor: "roxa"}];
+
+console.log(fruta);
+alert(fruta[1].cor);
+
+*/
